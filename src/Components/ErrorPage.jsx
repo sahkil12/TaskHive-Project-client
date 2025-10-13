7e760e13af1909c9813a7ca5e410be28665ee687
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-10-13 23:33:57 +0600

    added spinner and error page system

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -8,8 +8,8 @@ const ErrorPage = () => {
         <div>
             <Navbar></Navbar>
             <div className="my-8 min-h-[calc(100vh-432px)] flex flex-col items-center justify-center">
-                <Link to={'/'} className="text-xl btn py-6 btn-primary flex gap-2 items-center mt-6 pop text-center">  <FaArrowLeftLong size={23} /> Back to home</Link>
-                <img src="/404.gif" className="w-fit p-5 sm:p-10" alt="" />
+                <Link to={'/'} className="text-xl rounded-full px-6 btn py-6 btn-primary flex gap-2 items-center mt-7 pop text-center">  <FaArrowLeftLong size={23} /> Back to home</Link>
+                <img src="/404.gif" className="w-fit p-6 sm:p-10" alt="" />
             </div>
             <Footer></Footer>
         </div>
